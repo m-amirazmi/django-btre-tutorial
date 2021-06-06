8ae5c18eae7a470525b90e5c886643ca1ea9a31e
Muhamad Amir <60350614+m-amirazmi@users.noreply.github.com>
2021-06-06 21:47:00 +0800

    remove send mail functionality

--- contacts/views.py
+++ contacts/views.py
@@ -31,13 +31,13 @@ def contact(request):
         contact.save()
 
         # Send Email
-        send_mail(
-            'Property Listing Inquiry',
-            f'There has been an inquiry for {listing}. Sign into the admin panel for more info.',
-            'merbmi14@gmail.com',
-            [realtor_email, 'm.amirazmi@hotmail.com'],
-            fail_silently=False
-        )
+        # send_mail(
+        #     'Property Listing Inquiry',
+        #     f'There has been an inquiry for {listing}. Sign into the admin panel for more info.',
+        #     'merbmi14@gmail.com',
+        #     [realtor_email, 'm.amirazmi@hotmail.com'],
+        #     fail_silently=False
+        # )
 
         messages.success(
             request, 'Your request has been submitted, a realtor will get back to you soon')
